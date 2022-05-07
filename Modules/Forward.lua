3237ce7864ba816ecf0b22eeb700e03127ebbe4c
MailBuddy <105099878+MailBuddy82@users.noreply.github.com>
2022-05-07 15:37:21 +0200

    Update Forward.lua

--- Modules/Forward.lua
+++ Modules/Forward.lua
@@ -1,35 +1,35 @@
-local Postal = LibStub("AceAddon-3.0"):GetAddon("Postal")
-local Postal_Forward = Postal:NewModule("Forward", "AceHook-3.0", "AceEvent-3.0")
-local L = LibStub("AceLocale-3.0"):GetLocale("Postal")
-Postal_Forward.description = L["Allows you to forward the contents of a mail."]
-Postal_Forward.description2 = L[ [[|cFFFFCC00*|r Feature is not supported for mail sent with money attached or sent COD.
+local MailBuddy = LibStub("AceAddon-3.0"):GetAddon("MailBuddy")
+local MailBuddy_Forward = MailBuddy:NewModule("Forward", "AceHook-3.0", "AceEvent-3.0")
+local L = LibStub("AceLocale-3.0"):GetLocale("MailBuddy")
+MailBuddy_Forward.description = L["Allows you to forward the contents of a mail."]
+MailBuddy_Forward.description2 = L[ [[|cFFFFCC00*|r Feature is not supported for mail sent with money attached or sent COD.
 |cFFFFCC00*|r Feature is not supported for mail sent with stackable items attached.
 |cFFFFCC00*|r Forward button will be disabled in these cases.]] ]
 
-local PostalForwardTable = {}
+local MailBuddyForwardTable = {}
 
-function Postal_Forward:Postal_Forward_ForwardMailItemsEvent(event,...)
-	Postal_Forward_ForwardMailItems(2)
+function MailBuddy_Forward:MailBuddy_Forward_ForwardMailItemsEvent(event,...)
+	MailBuddy_Forward_ForwardMailItems(2)
 end
 
 -- Create Forward button and hook OnClick event
-function Postal_Forward:OnEnable()
-	if not PostalForwardButton then
-		PostalForwardButton = CreateFrame("Button", "OpenMailForwardButton", OpenMailFrame, "UIPanelButtonTemplate")
-		PostalForwardButton:SetWidth(82)
-		PostalForwardButton:SetHeight(22)
-		PostalForwardButton:SetPoint("RIGHT", "OpenMailReplyButton", "LEFT", 0, 0)
-		PostalForwardButton:SetText(L["Forward"])
-		PostalForwardButton:SetScript("OnClick", function() Postal_Forward_OpenMail_Forward() end)
-		PostalForwardButton:SetFrameLevel(PostalForwardButton:GetFrameLevel() + 1)
+function MailBuddy_Forward:OnEnable()
+	if not MailBuddyForwardButton then
+		MailBuddyForwardButton = CreateFrame("Button", "OpenMailForwardButton", OpenMailFrame, "UIPanelButtonTemplate")
+		MailBuddyForwardButton:SetWidth(82)
+		MailBuddyForwardButton:SetHeight(22)
+		MailBuddyForwardButton:SetPoint("RIGHT", "OpenMailReplyButton", "LEFT", 0, 0)
+		MailBuddyForwardButton:SetText(L["Forward"])
+		MailBuddyForwardButton:SetScript("OnClick", function() MailBuddy_Forward_OpenMail_Forward() end)
+		MailBuddyForwardButton:SetFrameLevel(PostalForwardButton:GetFrameLevel() + 1)
 	end
 	if OpenMailForwardButton then OpenMailForwardButton:Show() end
-	self:SecureHook("InboxFrame_OnClick", Postal_Forward_OpenMailFrameUpdated)
+	self:SecureHook("InboxFrame_OnClick", MailBuddy_Forward_OpenMailFrameUpdated)
 end
 
 -- Disabling modules unregisters all events/hook automatically
-function Postal_Forward:OnDisable()
-	Postal_Forward:UnregisterAllEvents()
+function MailBuddy_Forward:OnDisable()
+	MailBuddy_Forward:UnregisterAllEvents()
 	if OpenMailForwardButton then OpenMailForwardButton:Hide() end
 end
 
@@ -70,13 +70,13 @@ local function FreeBagSpace()
 end
 
 -- Uses Containers free space tables to find and return location of latest item added to inventory
-local function Postal_Inventory_Change(action)
+local function MailBuddy_Inventory_Change(action)
 	local bagID, Key
 	local TempTable = {}
 	if action == 1 then	-- take snap shot of current container free space tables and store
-		wipe(PostalForwardTable)
+		wipe(MailBuddyForwardTable)
 		for bagID = 0, 4, 1 do
-			table.insert(PostalForwardTable, GetContainerFreeSlots(bagID))
+			table.insert(MailBuddyForwardTable, GetContainerFreeSlots(bagID))
 		end
 		return 0, 0
 	end
@@ -85,8 +85,8 @@ local function Postal_Inventory_Change(action)
 		for bagID = 0, 4, 1 do
 			TempTable = GetContainerFreeSlots(bagID)
 			for Key = 1, #PostalForwardTable[bagID + 1], 1 do
-				if not contains(TempTable, PostalForwardTable[bagID + 1][Key]) then
-					return bagID, PostalForwardTable[bagID + 1][Key]
+				if not contains(TempTable, MailBuddyForwardTable[bagID + 1][Key]) then
+					return bagID, MailBuddyForwardTable[bagID + 1][Key]
 				end
 			end
 		end
@@ -94,7 +94,7 @@ local function Postal_Inventory_Change(action)
 end
 
 -- Enable/Disable Forward button as appropriate for current selected mail
-function Postal_Forward_OpenMailFrameUpdated()
+function MailBuddy_Forward_OpenMailFrameUpdated()
 	if not OpenMailFrame:IsVisible() then return end
 	OpenMailForwardButton:Enable()
 	local packageIcon, stationeryIcon, sender, subject, money, CODAmount, daysLeft, hasItem, wasRead, wasReturned, textCreated, canReply, isGM = GetInboxHeaderInfo(InboxFrame.openMailID)
@@ -108,7 +108,7 @@ function Postal_Forward_OpenMailFrameUpdated()
 end
 
 -- Generate Forward mail
-function Postal_Forward_OpenMail_Forward()
+function MailBuddy_Forward_OpenMail_Forward()
 	MailFrameTab_OnClick(nil, 2)
 	SendMailNameEditBox:SetText("")
 	local subject = OpenMailSubject:GetText()
@@ -120,11 +120,11 @@ function Postal_Forward_OpenMail_Forward()
 	if subject then SendMailSubjectEditBox:SetText(subject) end
 	if bodyText then SendMailBodyEditBox:SetText(bodyText) end
 	SendMailNameEditBox:SetFocus()
-	Postal_Forward_ForwardMailItems(1)
+	MailBuddy_Forward_ForwardMailItems(1)
 end
 
 -- Deal with attachments that need forwarded
-function Postal_Forward_ForwardMailItems(action)
+function MailBuddy_Forward_ForwardMailItems(action)
 	local bagID, itemID, itemIndex, hasItem
 	hasItem = select(8, GetInboxHeaderInfo(InboxFrame.openMailID))
 	if action == 1 then
@@ -132,19 +132,19 @@ function Postal_Forward_ForwardMailItems(action)
 		for itemIndex = 1, ATTACHMENTS_MAX_SEND do
 			itemID = select(2, GetInboxItem(InboxFrame.openMailID, itemIndex))
 			if itemID then
-				Postal_Inventory_Change(1)
-				Postal_Forward:RegisterEvent("BAG_UPDATE_DELAYED","Postal_Forward_ForwardMailItemsEvent")
+				MailBuddy_Inventory_Change(1)
+				MailBuddy_Forward:RegisterEvent("BAG_UPDATE_DELAYED","MailBuddy_Forward_ForwardMailItemsEvent")
 				TakeInboxItem(InboxFrame.openMailID, itemIndex)
 				return
 			end
 		end
 	end
 	if action == 2 then
-		Postal_Forward:UnregisterEvent("BAG_UPDATE_DELAYED","Postal_Forward_ForwardMailItemsEvent")
-		bagID, itemIndex = Postal_Inventory_Change(2)
+		MailBuddy_Forward:UnregisterEvent("BAG_UPDATE_DELAYED","MailBuddy_Forward_ForwardMailItemsEvent")
+		bagID, itemIndex = MailBuddy_Inventory_Change(2)
 		PickupContainerItem(bagID, itemIndex)
 		ClickSendMailItemButton()
-		Postal_Forward_ForwardMailItems(1)
+		MailBuddy_Forward_ForwardMailItems(1)
 		return
 	end
 end
