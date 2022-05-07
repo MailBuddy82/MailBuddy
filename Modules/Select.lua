a80a2f76258eb53c08c4938bf3c2dc0049559719
MailBuddy <105099878+MailBuddy82@users.noreply.github.com>
2022-05-07 15:33:27 +0200

    Update Select.lua

--- Modules/Select.lua
+++ Modules/Select.lua
@@ -1,8 +1,8 @@
-local Postal = LibStub("AceAddon-3.0"):GetAddon("Postal")
-local Postal_Select = Postal:NewModule("Select", "AceEvent-3.0", "AceHook-3.0")
-local L = LibStub("AceLocale-3.0"):GetLocale("Postal")
-Postal_Select.description = L["Add check boxes to the inbox for multiple mail operations."]
-Postal_Select.description2 = L[ [[|cFFFFCC00*|r Selected mail will be batch opened or returned to sender by clicking Open or Return.
+local MailBuddy = LibStub("AceAddon-3.0"):GetAddon("MailBuddy")
+local MailBuddy_Select = MailBuddy:NewModule("Select", "AceEvent-3.0", "AceHook-3.0")
+local L = LibStub("AceLocale-3.0"):GetLocale("MailBuddy")
+MailBuddy_Select.description = L["Add check boxes to the inbox for multiple mail operations."]
+MailBuddy_Select.description2 = L[ [[|cFFFFCC00*|r Selected mail will be batch opened or returned to sender by clicking Open or Return.
 |cFFFFCC00*|r You can Shift-Click 2 checkboxes to mass select every mail between the 2 checkboxes.
 |cFFFFCC00*|r You can Ctrl-Click a checkbox to mass select or unselect every mail from that sender.
 |cFFFFCC00*|r Select will never delete any mail (mail without text is auto-deleted by the game when all attached items and gold are taken).
@@ -16,7 +16,7 @@ local currentMode = nil
 local selectedMail = {}
 local openButton = nil
 local returnButton = nil
-local checkboxFunc = function(self) Postal_Select:ToggleMail(self) end
+local checkboxFunc = function(self) MailBuddy_Select:ToggleMail(self) end
 local mailIndex, attachIndex
 local lastItem, lastNumAttach, lastNumGold
 local wait
@@ -29,7 +29,7 @@ local mailID = {}
 local updateFrame = CreateFrame("Frame")
 updateFrame:Hide()
 updateFrame:SetScript("OnShow", function(self)
-	self.time = Postal.db.profile.OpenSpeed
+	self.time = MailBuddy.db.profile.OpenSpeed
 	if invAlmostFull and self.time < 1.0 and not self.lootingMoney then
 		-- Delay opening to 1 second to account for a nearly full
 		-- inventory to respect the KeepFreeSpace setting
@@ -41,7 +41,7 @@ updateFrame:SetScript("OnUpdate", function(self, elapsed)
 	self.time = self.time - elapsed
 	if self.time <= 0 then
 		self:Hide()
-		Postal_Select:ProcessNext()
+		MailBuddy_Select:ProcessNext()
 	end
 end)
 
@@ -65,33 +65,33 @@ local function printTooMuchMail()
 	local timeLeft = lastRefill+60-GetTime()
 	if cur>=100 or -- if inbox is full, no more will arrive
 	   timeLeft<0 then	-- if someone waited more than 60 seconds to take a mail out....
-		Postal:Print(format(L["There are %i more messages not currently shown."], tot-cur))
+		MailBuddy:Print(format(L["There are %i more messages not currently shown."], tot-cur))
 	else
-		Postal:Print(format(L["There are %i more messages not currently shown. More should become available in %i seconds."], tot-cur, timeLeft))
+		MailBuddy:Print(format(L["There are %i more messages not currently shown. More should become available in %i seconds."], tot-cur, timeLeft))
 	end
 
 end
 
-function Postal_Select:OnEnable()
+function MailBuddy_Select:OnEnable()
 	--create the open button
 	if not openButton then
-		openButton = CreateFrame("Button", "PostalSelectOpenButton", InboxFrame, "UIPanelButtonTemplate")
+		openButton = CreateFrame("Button", "MailBuddySelectOpenButton", InboxFrame, "UIPanelButtonTemplate")
 		openButton:SetWidth(120)
 		openButton:SetHeight(25)
 		openButton:SetPoint("RIGHT", InboxFrame, "TOP", 0, -42)
 		openButton:SetText(L["Open"])
-		openButton:SetScript("OnClick", function() Postal_Select:HandleSelect(1) end)
+		openButton:SetScript("OnClick", function() MailBuddy_Select:HandleSelect(1) end)
 		openButton:SetFrameLevel(openButton:GetFrameLevel() + 1)
 	end
 
 	--create the return button
 	if not returnButton then
-		returnButton = CreateFrame("Button", "PostalSelectReturnButton", InboxFrame, "UIPanelButtonTemplate")
+		returnButton = CreateFrame("Button", "MailBuddySelectReturnButton", InboxFrame, "UIPanelButtonTemplate")
 		returnButton:SetWidth(120)
 		returnButton:SetHeight(25)
 		returnButton:SetPoint("LEFT", InboxFrame, "TOP", 5, -42)
 		returnButton:SetText(L["Return"])
-		returnButton:SetScript("OnClick", function() Postal_Select:HandleSelect(2) end)
+		returnButton:SetScript("OnClick", function() MailBuddy_Select:HandleSelect(2) end)
 		returnButton:SetFrameLevel(returnButton:GetFrameLevel() + 1)
 	end
 
@@ -104,15 +104,15 @@ function Postal_Select:OnEnable()
 
 	--now create the checkboxes
 	for i = 1, 7 do
-		if not _G["PostalInboxCB"..i] then
-			local CB = CreateFrame("CheckButton", "PostalInboxCB"..i, _G["MailItem"..i], "OptionsCheckButtonTemplate")
+		if not _G["MailBuddyInboxCB"..i] then
+			local CB = CreateFrame("CheckButton", "MailBuddyInboxCB"..i, _G["MailItem"..i], "OptionsCheckButtonTemplate")
 			CB:SetID(i)
 			CB:SetPoint("RIGHT", "MailItem"..i, "LEFT", 1, -5)
 			CB:SetWidth(24)
 			CB:SetHeight(24)
 			CB:SetHitRectInsets(0, 0, 0, 0)
 			CB:SetScript("OnClick", checkboxFunc)
-			local text = CB:CreateFontString("PostalInboxCB"..i.."Text", "BACKGROUND", "GameFontHighlightSmall")
+			local text = CB:CreateFontString("MailBuddyInboxCB"..i.."Text", "BACKGROUND", "GameFontHighlightSmall")
 			text:SetPoint("BOTTOM", CB, "TOP")
 			text:SetText(i)
 			CB.text = text
@@ -131,11 +131,11 @@ function Postal_Select:OnEnable()
 	openButton:Show()
 	returnButton:Show()
 	for i = 1, 7 do
-		_G["PostalInboxCB"..i]:Show()
+		_G["MailBuddyInboxCB"..i]:Show()
 	end
 end
 
-function Postal_Select:OnDisable()
+function MailBuddy_Select:OnDisable()
 	self:Reset()
 	if self:IsHooked("InboxFrame_Update") then
 		self:Unhook("InboxFrame_Update")
@@ -151,14 +151,14 @@ function Postal_Select:OnDisable()
 	InboxTooMuchMail.Show = nil
 end
 
-function Postal_Select:MAIL_SHOW()
+function MailBuddy_Select:MAIL_SHOW()
 	self:RegisterEvent("MAIL_CLOSED", "Reset")
 	self:RegisterEvent("PLAYER_LEAVING_WORLD", "Reset")
 	self:RegisterEvent("MAIL_INBOX_UPDATE")
 	self:BuildUniqueIDs()
 end
 
-function Postal_Select:ToggleMail(frame)
+function MailBuddy_Select:ToggleMail(frame)
 	local index = frame:GetID() + (InboxFrame.pageNum - 1) * 7
 	if lastCheck and IsShiftKeyDown() then
 		-- Sneaky feature to shift-click a checkbox to select every
@@ -191,7 +191,7 @@ function Postal_Select:ToggleMail(frame)
 	end
 end
 
-function Postal_Select:GetUniqueID(index)
+function MailBuddy_Select:GetUniqueID(index)
 	local packageIcon, stationeryIcon, sender, subject, money, CODAmount, daysLeft, hasItem, wasRead, wasReturned, textCreated, canReply, isGM = GetInboxHeaderInfo(index)
 	packageIcon = packageIcon or ""
 	stationeryIcon = stationeryIcon or ""
@@ -205,7 +205,7 @@ function Postal_Select:GetUniqueID(index)
 	return format("%s%s%s%s%s%s%d%d%d%d%d", packageIcon, stationeryIcon, sender, subject, money, CODAmount, hasItem, wasReturned, textCreated, canReply, isGM)
 end
 
-function Postal_Select:BuildUniqueIDs()
+function MailBuddy_Select:BuildUniqueIDs()
 	-- Build a unique ID for every mail
 	local numMails = GetInboxNumItems()
 	wipe(mailID)
@@ -214,7 +214,7 @@ function Postal_Select:BuildUniqueIDs()
 	end
 end
 
-function Postal_Select:HandleSelect(mode)
+function MailBuddy_Select:HandleSelect(mode)
 	mailIndex = GetInboxNumItems() or 0
 	attachIndex = ATTACHMENTS_MAX_RECEIVE
 	invFull = nil
@@ -238,14 +238,14 @@ function Postal_Select:HandleSelect(mode)
 	end
 
 	--protect the user from changing anything while were in process
-	Postal:DisableInbox(1)
+	MailBuddy:DisableInbox(1)
 	if self:IsHooked("InboxFrame_Update") then
 		self:Unhook("InboxFrame_Update")
 	end
 
 	for i = 1, 7 do
 		local index = i + (InboxFrame.pageNum-1) * 7
-		local CB = _G["PostalInboxCB"..i]
+		local CB = _G["MailBuddyInboxCB"..i]
 		CB:Hide()
 	end
 
@@ -253,7 +253,7 @@ function Postal_Select:HandleSelect(mode)
 	self:ProcessNext()
 end
 
-function Postal_Select:ProcessNext()
+function MailBuddy_Select:ProcessNext()
 	-- Skip mails not selected to be processed
 	while not selectedMail[mailIndex] and mailIndex > 0 do
 		mailIndex = mailIndex - 1
@@ -268,7 +268,7 @@ function Postal_Select:ProcessNext()
 
 			-- Check if we need to wait for the mailbox to change
 			if wait then
-				local attachCount, goldCount = Postal:CountItemsAndMoney()
+				local attachCount, goldCount = MailBuddy:CountItemsAndMoney()
 				if lastNumGold ~= goldCount then
 					-- Process next mail, gold has been taken
 					wait = false
@@ -296,16 +296,16 @@ function Postal_Select:ProcessNext()
 			end
 
 			-- Print message on next mail
-			if Postal.db.profile.Select.SpamChat and attachIndex == ATTACHMENTS_MAX_RECEIVE then
+			if MailBuddy.db.profile.Select.SpamChat and attachIndex == ATTACHMENTS_MAX_RECEIVE then
 				if not invFull or msgMoney > 0 then
-					local moneyString = msgMoney > 0 and " ["..Postal:GetMoneyString(msgMoney).."]" or ""
+					local moneyString = msgMoney > 0 and " ["..MailBuddy:GetMoneyString(msgMoney).."]" or ""
 					local playerName
-					local mailType = Postal:GetMailType(msgSubject)
+					local mailType = MailBuddy:GetMailType(msgSubject)
 					if (mailType == "AHSuccess" or mailType == "AHWon") then
 						playerName = select(3,GetInboxInvoiceInfo(mailIndex))
 						playerName = playerName and (" ("..playerName..")")
 					end
-					Postal:Print(format("%s %d: %s%s%s", L["Open"], mailIndex, msgSubject or "", moneyString, (playerName or "")))
+					MailBuddy:Print(format("%s %d: %s%s%s", L["Open"], mailIndex, msgSubject or "", moneyString, (playerName or "")))
 				end
 			end
 
@@ -324,7 +324,7 @@ function Postal_Select:ProcessNext()
 			end
 
 			-- Check for free bag space
-			if attachIndex > 0 and not invFull and Postal.db.profile.Select.KeepFreeSpace > 0 then
+			if attachIndex > 0 and not invFull and MailBuddy.db.profile.Select.KeepFreeSpace > 0 then
 				local free = 0
 				for bag = 0, NUM_BAG_SLOTS do
 					local bagFree, bagFam = GetContainerNumFreeSlots(bag)
@@ -332,11 +332,11 @@ function Postal_Select:ProcessNext()
 						free = free + bagFree
 					end
 				end
-				if free <= Postal.db.profile.Select.KeepFreeSpace then
+				if free <= MailBuddy.db.profile.Select.KeepFreeSpace then
 					invFull = true
 					invAlmostFull = nil
-					Postal:Print(format(L["Not taking more items as there are now only %d regular bagslots free."], free))
-				elseif free <= Postal.db.profile.Select.KeepFreeSpace + 1 then
+					MailBuddy:Print(format(L["Not taking more items as there are now only %d regular bagslots free."], free))
+				elseif free <= MailBuddy.db.profile.Select.KeepFreeSpace + 1 then
 					invAlmostFull = true
 				end
 			end
@@ -367,10 +367,10 @@ function Postal_Select:ProcessNext()
 
 			if attachIndex > 0 and (lootFlag or not invFull) then
 				-- If there's attachments, take the item
-				--Postal:Print("Getting Item from Message "..mailIndex..", "..attachIndex)
+				--MailBuddy:Print("Getting Item from Message "..mailIndex..", "..attachIndex)
 				TakeInboxItem(mailIndex, attachIndex)
 
-				lastNumAttach, lastNumGold = Postal:CountItemsAndMoney()
+				lastNumAttach, lastNumGold = MailBuddy:CountItemsAndMoney()
 				wait = true
 				-- Find next attachment index backwards
 				local attachIndex2 = attachIndex - 1
@@ -382,10 +382,10 @@ function Postal_Select:ProcessNext()
 				updateFrame:Show()
 			elseif msgMoney > 0 then
 				-- No attachments, but there is money
-				--Postal:Print("Getting Gold from Message "..mailIndex)
+				--MailBuddy:Print("Getting Gold from Message "..mailIndex)
 				TakeInboxMoney(mailIndex)
 
-				lastNumAttach, lastNumGold = Postal:CountItemsAndMoney()
+				lastNumAttach, lastNumGold = MailBuddy:CountItemsAndMoney()
 				wait = true
 
 				updateFrame.lootingMoney = true
@@ -400,7 +400,7 @@ function Postal_Select:ProcessNext()
 
 		else
 			-- Return mode
-			if Postal.db.profile.Select.SpamChat and attachIndex == ATTACHMENTS_MAX_RECEIVE then
+			if MailBuddy.db.profile.Select.SpamChat and attachIndex == ATTACHMENTS_MAX_RECEIVE then
 				Postal:Print(L["Return"].." "..mailIndex..": "..msgSubject)
 			end
 			if not InboxItemCanDelete(mailIndex) then
@@ -410,7 +410,7 @@ function Postal_Select:ProcessNext()
 				mailIndex = mailIndex - 1
 				attachIndex = ATTACHMENTS_MAX_RECEIVE
 			else
-				Postal:Print(L["Skipping"].." "..mailIndex..": "..msgSubject)
+				MailBuddy:Print(L["Skipping"].." "..mailIndex..": "..msgSubject)
 				mailIndex = mailIndex - 1
 				attachIndex = ATTACHMENTS_MAX_RECEIVE
 				return self:ProcessNext() -- tail call
@@ -419,16 +419,16 @@ function Postal_Select:ProcessNext()
 
 	else
 		-- Reached the end of opening all selected mail
-		if skipFlag then Postal:Print(L["Some Messages May Have Been Skipped."]) end
+		if skipFlag then MailBuddy:Print(L["Some Messages May Have Been Skipped."]) end
 		self:Reset()
 	end
 end
 
-function Postal_Select:InboxFrame_Update()
+function MailBuddy_Select:InboxFrame_Update()
 	self.hooks["InboxFrame_Update"]()
 	for i = 1, 7 do
 		local index = i + (InboxFrame.pageNum-1)*7
-		local CB = _G["PostalInboxCB"..i]
+		local CB = _G["MailBuddyInboxCB"..i]
 		if index > GetInboxNumItems() then
 			CB:Hide()
 		else
@@ -439,8 +439,8 @@ function Postal_Select:InboxFrame_Update()
 	end
 end
 
-function Postal_Select:MAIL_INBOX_UPDATE()
-	--Postal:Print("update")
+function MailBuddy_Select:MAIL_INBOX_UPDATE()
+	-- MailBuddy:Print("update")
 	-- We need this because MAIL_INBOX_UPDATEs can now potentially
 	-- include mailbox refreshes since patch 4.0.3 (that is mail can
 	-- get inserted both at the back (old mail past 100) and at the front
@@ -471,7 +471,7 @@ function Postal_Select:MAIL_INBOX_UPDATE()
 				end
 			end
 		end
-		--Postal:Print(numNewMailsAtFront.. " new mails detected at front")
+		--MailBuddy:Print(numNewMailsAtFront.. " new mails detected at front")
 		if numNewMailsAtFront > 0 then
 			if mailIndex then
 				mailIndex = mailIndex + numNewMailsAtFront
@@ -492,7 +492,7 @@ function Postal_Select:MAIL_INBOX_UPDATE()
 	end
 end
 
-function Postal_Select:Reset(event)
+function MailBuddy_Select:Reset(event)
 	if not self:IsHooked("InboxFrame_Update") then self:RawHook("InboxFrame_Update", true) end
 
 	updateFrame:Hide()
@@ -500,7 +500,7 @@ function Postal_Select:Reset(event)
 
 	wipe(selectedMail)
 
-	Postal:DisableInbox()
+	MailBuddy:DisableInbox()
 	self:InboxFrame_Update()
 	openButton:SetText(L["Open"])
 	openButton:Show()
@@ -516,7 +516,7 @@ function Postal_Select:Reset(event)
 	InboxTooMuchMail.Show = printTooMuchMail
 end
 
-function Postal_Select:UI_ERROR_MESSAGE(event, error_message)
+function MailBuddy_Select:UI_ERROR_MESSAGE(event, error_message)
 	if error_message == ERR_INV_FULL then
 		invFull = true
 		wait = false
@@ -526,11 +526,11 @@ function Postal_Select:UI_ERROR_MESSAGE(event, error_message)
 	end
 end
 
-function Postal_Select.SetKeepFreeSpace(dropdownbutton, arg1, arg2, checked)
-	Postal.db.profile.Select.KeepFreeSpace = arg1
+function MailBuddy_Select.SetKeepFreeSpace(dropdownbutton, arg1, arg2, checked)
+	MailBuddy.db.profile.Select.KeepFreeSpace = arg1
 end
 
-function Postal_Select.ModuleMenu(self, level)
+function MailBuddy_Select.ModuleMenu(self, level)
 	if not level then return end
 	local info = self.info
 	wipe(info)
@@ -548,17 +548,17 @@ function Postal_Select.ModuleMenu(self, level)
 		info.text = L["Verbose mode"]
 		info.hasArrow = nil
 		info.value = nil
-		info.func = Postal.SaveOption
+		info.func = MailBuddy.SaveOption
 		info.arg1 = "Select"
 		info.arg2 = "SpamChat"
-		info.checked = Postal.db.profile.Select.SpamChat
+		info.checked = MailBuddy.db.profile.Select.SpamChat
 		info.isNotRadio = 1
 		UIDropDownMenu_AddButton(info, level)
 	elseif level == 2 + self.levelAdjust then
 		if UIDROPDOWNMENU_MENU_VALUE == "KeepFreeSpace" then
-			local keepFree = Postal.db.profile.Select.KeepFreeSpace
-			info.func = Postal_Select.SetKeepFreeSpace
-			for _, v in ipairs(Postal.keepFreeOptions) do
+			local keepFree = MailBuddy.db.profile.Select.KeepFreeSpace
+			info.func = MailBuddy_Select.SetKeepFreeSpace
+			for _, v in ipairs(MailBuddy.keepFreeOptions) do
 				info.text = v
 				info.checked = v == keepFree
 				info.arg1 = v
